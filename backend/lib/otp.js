68f4737534b6c0d40323382819afb79134ffdab8
surajarya1999 <surajarya460@gmail.com>
2026-03-14 11:07:12 +0530

    updation

--- backend/lib/otp.js
+++ backend/lib/otp.js
@@ -25,8 +25,16 @@ async function sendOTPEmail(email, otp, name) {
   console.log("🔑 EMAIL_USER:", process.env.EMAIL_USER);
   console.log("✅ OTP is:", otp);
   const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: { user: process.env.EMAIL_USER, pass: process.env.EMAIL_PASS },
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS
+    },
+    tls: {
+      rejectUnauthorized: false
+    }
   });
 
   await transporter.sendMail({
